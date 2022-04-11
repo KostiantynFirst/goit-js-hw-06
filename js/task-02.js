c4011ef21e4dba44f1e94de21c749f15feaf06c6
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2022-04-11 22:20:23 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = ingredients.map(ingredient => {
+ const el = document.createElement('li');
+  el.textContent = ingredient;
+  el.classList.add('item');
+  return el;
+});
+
+const ingredientsList = document.querySelector('ul#ingredients');
+ingredientsList.append(...ingredientsEl);
+
+
+// console.log(el[0].classList.add('item'));
+
+
+// el1 = ingredientsEl.classList.add('item');
+// console.log(el1);
+
+
+// ingredientsEl.classList.add(item);
+// console.log(ingredientsEl);
