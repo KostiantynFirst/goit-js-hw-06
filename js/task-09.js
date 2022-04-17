a330394e071318af8b2201868eb84abbcc472ca6
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2022-04-17 17:16:30 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const backgroundColor = document.querySelector('.color');
+const changeBackgroundColor = document.querySelector('.change-color');
+
+
+changeBackgroundColor.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  event.preventDefault();
+
+  const randomColor = getRandomHexColor();
+  
+  body.style.backgroundColor = randomColor;
+  backgroundColor.textContent = randomColor;
+  // console.log(getRandomHexColor());
+}
